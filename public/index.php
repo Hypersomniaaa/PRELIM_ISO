e46ab7826605d673d664e9b5184723dd87d5c6a4
Iso-IntegProg <135562542+Hypersomniaaa@users.noreply.github.com>
2024-02-23 13:43:23 +0800

    Add files via upload

---
+++ public/index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>LEGO | IMAGINATION MO ANG LIMIT</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+</head>
+<body>
+<header class="main_header">
+        <div class="header_top">
+            <h1 class="logo">LEGO</h1>
+            <a href="#" class="btn btn-rounded">Sign In</a>
+        </div>
+        <div class="header_content">
+            <h1>LEGO VS ROBLOX VS MINECRAFT</h1>
+            <p>Maverick Iso<br>BSIT-3A</p>
+            <a href="#about" class="btn btn-xl">
+                Learn More
+                <i class="fas fa-chevron-right btn-icon"></i>
+            </a>
+        </div>
+           
+    </header>
+
+
+     
+
+    <section class="gallery">
+        <div class="gallery-container">    
+            
+            <a href="#">
+                <div class="img-container">
+                    <img src="../public/img/1.jpg">
+                    <div class="text-container">
+                      <p> pwede sa LGBT</p>
+                    </div>
+                </div>
+                
+            </a>
+            
+            <a href="#">
+                <div class="img-container">
+                    <img src="../public/img/2.jpg" alt="snail1">
+                    <div class="text-container">
+                        <p> Pwede sa matandang baliw</p>
+                      </div>
+                </div>
+            </a>
+
+            <a href="#">
+                <div class="img-container">
+                    <img src="../public/img/3.jpg" alt="snail1">
+                    <div class="text-container">
+                        <p> pwede sa walang PC</p>
+                      </div>
+                </div>       
+            </a>
+
+ 
+          
+        </div>
+    </section>
+
+
+   
+    <section class="section-b" id="about">
+        <div class="overlay">
+            <div class="section-b-inner"> <br><br><br><br>
+                <h3> TENEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEN!!!!!!!!!!!!!!!!!!</h3>
+                <h2>aaaaaaaaaaaAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA!!!!!!</h2>
+                <p>dasjhdkljahsdlghiluwdbashdiouahw</p>
+            </div>
+        </div>
+    </section>
+
+
+   
+
+    
+
+
+    
+
+</body>
+</html>
+
+  
